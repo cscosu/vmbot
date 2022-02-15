61270cf982c750aa845658e7cae09456a2bb2427
gsemaj <gentsemaj@live.com>
2022-02-15 17:26:17 -0500

    Add VM assignment functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
-const { Client, Intents } = require('discord.js');
-const { token, clientID } = require('./config.json');
+const { Client, Intents, MessageActionRow, MessageButton } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.DIRECT_MESSAGES] });
 const args = process.argv.slice(2);
@@ -50,13 +50,57 @@ function releaseAccount(accountID) {
 }
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+    console.log(`[INIT] Logged in as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'vm') {
+        const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('release')
+					.setLabel('I\'m done using the VM')
+					.setStyle('DANGER'),
+			);
+
+        if(users[interaction.user.id] == undefined) {
+            var details = reserveAccount();
+            users[interaction.user.id] = details.id;
+            var str = `I reserved you a VM on https://vm.pwn.osucyber.club/\n`
+                        + `Username: \`${details.username}\`\n`
+                        + `Password: \`${details.password}\`\n`
+                        + `Let me know when you're done with it!`;
+            var message = {
+                content: str,
+                components: [row]
+            };
+            console.log(`[INFO] Assigned VM ${details.id} to client ${interaction.user.id} (${interaction.user.tag})`);
+        } else {
+            var message = "You already have a VM!";
+        }
+        
+
+        if(interaction.member == null) {
+            // in DMs already
+            interaction.reply(message);
+        } else {
+            // in server
+            interaction.member.createDM()
+                .then(channel => channel.send(message));
+                interaction.reply("Check your DMs!");
+        }
+	}
 });
 
 var loc = args[0] != undefined ? args[0] : "logins.txt";
 var available = [];
 var reserved = [];
+var users = {};
 
 available = loadLogins(loc);
-console.log(`Loaded ${available.length} accounts`);
+console.log(`[INIT] Loaded ${available.length} accounts`);
 client.login(token);
